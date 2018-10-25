f15e1acf27bd4aa5bf69716f89027a2acdf41396
OswinGuai <peizhyi@gmail.com>
2018-10-25 17:24:47 +0800

    init

---
+++ list_users.py
@@ -0,0 +1,34 @@
+import re
+import sys
+import os
+import pwd
+
+result_file = sys.argv[1]
+with open(result_file) as result:
+    lines = result.readlines()
+
+    if len(lines) < 10:
+        print('error result')
+        sys.exit()
+
+    for i,line in enumerate(lines):
+        if 'Processes:' in line:
+            break
+    if i == 0:
+        print('error result')
+        sys.exit()
+    start = i + 3
+    processes = lines[start:-1]
+    occupy = {}
+    for p in processes: 
+        m = re.split('\s+',p)
+        gpu = m[1]
+        pid = m[2]
+        proc_stat_file = os.stat("/proc/%d" % int(pid))
+        uid = proc_stat_file.st_uid
+        username = pwd.getpwuid(uid)[0]
+        if gpu not in occupy:
+            occupy[gpu] = []
+        occupy[gpu].append(username)
+    print(occupy)
+
---
+++ show_users.sh
@@ -0,0 +1,4 @@
+
+rm result.txt
+nvidia-smi >> result.txt
+python list_users.py result.txt
