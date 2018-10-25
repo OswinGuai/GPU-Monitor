1200a3c4c6147594400d9e80942299ca655b463b
OswinGuai <peizhyi@gmail.com>
2018-10-26 00:16:30 +0800

    update

--- show_users.sh
+++ show_users.sh
@@ -1,4 +1,6 @@
 
-rm result.txt
-nvidia-smi >> result.txt
-python list_users.py result.txt
+script_dir=$(dirname $0)
+
+rm $script_dir/result.txt
+nvidia-smi >> $script_dir/result.txt
+python $script_dir/list_users.py $script_dir/result.txt
