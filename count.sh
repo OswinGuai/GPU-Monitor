169f1c73314dfca589a7efc5aafb7dd03859fb1e
OswinGuai <peizhyi@gmail.com>
2018-11-28 11:39:15 +0800

    update

--- count.sh
+++ count.sh
@@ -1,6 +1,6 @@
 script_dir=$(dirname $0)
 
-for device in $(seq 101 1 111)
+for device in $(seq 101 1 106; seq 108 1 111)
 do
     ssh peizhongyi@192.168.6.$device 'export PATH=/workspace/peizhongyi/programs/GPU-Monitor:$PATH; submit_records.sh'
     rm $script_dir/result_$device.txt
